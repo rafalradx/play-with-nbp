8348c5d73ba7ee6d8005f6a0c578f92e483ba466
Rafal Pietras <rafal.radx@gmail.com>
2024-02-04 23:01:02 +0100

    dynamic asyncion.gather

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ import platform
 import aiohttp
 import asyncio
 import sys
+import json
 from datetime import date, timedelta
 
 
 def prepare_day_list(daysback):
     today = date.today()
     if daysback > 10:
-        print("Currency rate check limited to 10 days back")
+        print("Currency exchange-rate check limited to 10 days back")
         daysback = 10
     return [str(today - timedelta(days=n)) for n in range(daysback + 1)]
 
@@ -25,25 +26,30 @@ async def get_page(day, session):
 
 
 async def main():
-    try:
-        daysback = int(sys.argv[1])
 
-        days = prepare_day_list(daysback)
-        print(days)
+    if len(sys.argv) > 1:
+        try:
+            daysback = int(sys.argv[1])
+            days = prepare_day_list(daysback)
+            print(days)
 
-    except ValueError as err:
-        print(str(err), "\nArgument should be (int)")
-        return -1
-    except IndexError as err:
-        print(str(err), "\nPlease provide at least one argument (int)")
-        return -1
+        except ValueError as err:
+            print(str(err), "\nArgument should be (int)")
+            return -1
+    else:
+        days = prepare_day_list(0)
 
     async with aiohttp.ClientSession() as session:
-        result = await asyncio.gather(
-            get_page(days[0], session),
-            get_page(days[1], session),
-            get_page(days[2], session),
-        )
+        result = await asyncio.gather(*[get_page(day, session) for day in days])
+        #     get_page(days[0], session),
+        #     get_page(days[1], session),
+        #     get_page(days[2], session),
+        # )
+        # result = await asyncio.gather(
+        #     get_page(days[0], session),
+        #     get_page(days[1], session),
+        #     get_page(days[2], session),
+        # )
         return result
 
 
@@ -53,3 +59,4 @@ if __name__ == "__main__":
         asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
     r = asyncio.run(main())
     print(r)
+    # print(json.dumps(r, indent=4))
